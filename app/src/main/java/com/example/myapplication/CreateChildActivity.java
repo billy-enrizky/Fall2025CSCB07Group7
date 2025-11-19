85fce8862d211812f4f4471e393faa665c8afc28
Kingsley Chan <chankingsleykt@gmail.com>
2025-11-19 11:54:42 -0500

    added CreateChildActivity, related xml file, refactored ChildAccount hierarchy to reflect child account data needs

---
+++ app/src/main/java/com/example/myapplication/CreateChildActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateChildActivity extends AppCompatActivity {
+    private EditText childNameEditText;
+    private CalendarView childDobCalendarView;
+    private EditText childAgeEditText;
+    private EditText childNotesEditText;
+    private Button createChildButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_child_account);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/myapplication/userdata/ChildAccount.java
+++ app/src/main/java/com/example/myapplication/userdata/ChildAccount.java
@@ -1,14 +1,34 @@
 package com.example.myapplication.userdata;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.myapplication.CallBack;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+
+import java.time.LocalDate;
+import java.util.Map;
+
 public class ChildAccount extends UserData {
     String Parent_id;
-    public ChildAccount() {
-        super();
-        Parent_id = "";
-    }
+    LocalDate dob;
+    int age;
     public ChildAccount(String ID) {
         super(ID);
-        Parent_id = "";
     }
-
+    public void setDob(int year, int month, int day) {
+        this.dob = LocalDate.of(year, month, day);
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public void WriteIntoDatabase(DatabaseReference mDatabase) {
+        super.WriteIntoDatabase(mDatabase);
+    }
 }
