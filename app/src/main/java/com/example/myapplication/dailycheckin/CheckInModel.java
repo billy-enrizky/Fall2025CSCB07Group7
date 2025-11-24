a78d028b61ebab43a7461c512136228b191e33cf
Yipeng Zhao <passione158@gmail.com>
2025-11-24 01:07:57 -0500

    feat: add comment and doc for use

--- app/src/main/java/com/example/myapplication/dailycheckin/CheckInModel.java
+++ app/src/main/java/com/example/myapplication/dailycheckin/CheckInModel.java
@@ -19,13 +19,20 @@ import java.util.HashMap;
 public class CheckInModel {
     public CheckInModel() {
     }
-    public String getDate(){
+    public static String getDate(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             return LocalDate.now().toString();
         }
         return null;
     }
-    public void WriteIntoDB(DailyCheckin checkin, CallBack callback){
+
+    //Below are severall few methods for your use.
+
+    //This method Write one single DailyCheckin into the database.
+    //I didn't use the "users" node, but the a new node "CheckInManager"
+    //Because we need to read "users" when sign in,
+    //I don't wanna make sign in too slow.
+    public static void WriteIntoDB(DailyCheckin checkin, CallBack callback){
         String Date = getDate();
         UserManager.mDatabase.child("CheckInManager")
                 .child(checkin.username).child(Date).setValue(checkin)
@@ -39,8 +46,18 @@ public class CheckInModel {
         });
     }
 
-
-    public void readFromDB(String username, ResultCallBack<HashMap<String,DailyCheckin>> callback){
+ /*     The way to use reading method is as follows:
+        Say you want to read the DB, and then use the info to do thing A
+        CheckInModel.readFromDB(username, new ResultCallBack<HashMap<String,DailyCheckin>>(){
+            @Override
+            public void onComplete(HashMap<String,DailyCheckin> result){
+                                <Here are the info you get (result)↑ >
+                  <attach A here>
+            }
+        });
+        <It's incorrect to attach A here, as reading might haven't done>
+*/
+    public static void readFromDB(String username, ResultCallBack<HashMap<String,DailyCheckin>> callback){
         UserManager.mDatabase.child("CheckInManager").child(username)
             .addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -60,7 +77,7 @@ public class CheckInModel {
         });
     }
 
-    public void readFromDB(String username,String Date, ResultCallBack<DailyCheckin> callback){
+    public static void readFromDB(String username,String Date, ResultCallBack<DailyCheckin> callback){
         UserManager.mDatabase.child("CheckInManager").child(username).child(Date)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -76,7 +93,7 @@ public class CheckInModel {
                 });
     }
 
-    public void readFromDB(String username,String begin, String end, ResultCallBack<HashMap<String,DailyCheckin>> callback){
+    public static void readFromDB(String username,String begin, String end, ResultCallBack<HashMap<String,DailyCheckin>> callback){
         UserManager.mDatabase.child("CheckInManager").child(username)
                 .orderByKey()
                 .startAt(begin)     // eg. "2025-01-01"
