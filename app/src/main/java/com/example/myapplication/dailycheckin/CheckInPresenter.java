80f25f5dbde1862327da03f105ee0d89df893dae
Kingsley Chan <chankingsleykt@gmail.com>
2025-11-25 06:51:51 -0500

    feat: started mvp system for daily check-in page

---
+++ app/src/main/java/com/example/myapplication/dailycheckin/CheckInPresenter.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.dailycheckin;
+
+import static android.text.TextUtils.isEmpty;
+import static android.widget.Toast.makeText;
+
+import static java.util.Objects.isNull;
+
+import android.widget.Toast;
+
+import com.example.myapplication.CallBack;
+import com.example.myapplication.ResultCallBack;
+import com.example.myapplication.SignUpActivity;
+import com.example.myapplication.UserManager;
+import com.example.myapplication.userdata.AccountType;
+import com.example.myapplication.userdata.UserData;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
+public class CheckInPresenter {
+    static CheckInView view;
+    static CheckInModel model;
+    public CheckInPresenter (CheckInView view, CheckInModel model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    void initialize(){
+        //model.ReloadUserAuth();
+    }
+
+    static void logEntry(String username, boolean nightWaking, String activityLimits, double coughWheezeLevel, ArrayList<String> triggers) {
+        DailyCheckin entry = new DailyCheckin(username, nightWaking, activityLimits, coughWheezeLevel, triggers);
+        model.WriteIntoDB(entry, new CallBack() {
+            @Override
+            public void onComplete() {
+                view.showShortMessage("Entry successfully logged!");
+            }
+        });
+    }
+}
