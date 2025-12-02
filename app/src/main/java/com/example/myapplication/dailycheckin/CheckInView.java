d52abee0392e4640fdf349c36073fe5457202440
Kingsley Chan <chankingsleykt@gmail.com>
2025-12-02 10:08:07 -0500

    started making child ui consistently green

--- app/src/main/java/com/example/myapplication/dailycheckin/CheckInView.java
+++ app/src/main/java/com/example/myapplication/dailycheckin/CheckInView.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication.ChildActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.UserManager;
 import com.example.myapplication.childmanaging.SignInChildProfileActivity;
