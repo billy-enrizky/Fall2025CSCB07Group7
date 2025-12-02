80088c14d4f0318aaa3ce2d5b3c3961d5758cedc
billy-enrizky <billy.suharno@mail.utoronto.ca>
2025-12-01 20:09:40 -0500

    link controller dates to streak

--- app/src/main/java/com/example/myapplication/inhaler/ChildInhalerUseAfter.java
+++ app/src/main/java/com/example/myapplication/inhaler/ChildInhalerUseAfter.java
@@ -119,17 +119,28 @@ public class ChildInhalerUseAfter extends AppCompatActivity {
                                                         Toast.makeText(ChildInhalerUseAfter.this, "Warning: Achievement Error.", Toast.LENGTH_SHORT).show();
                                                         startActivity(new Intent(ChildInhalerUseAfter.this, ChildInhalerMenu.class));
                                                     } else {
-                                                        achievement.updateStreak();/*
-                                                        if (!achievement.badges.get(0)) {
-                                                            if (achievement.checkBadge1()) {
-                                                                Toast.makeText(this, "You've earned a new badge!", Toast.LENGTH_SHORT).show();
-                                                                achievement.badges.set(0, true);
-                                                            }
-                                                        }*/
-                                                        AchievementsModel.writeIntoDB(achievement, new CallBack() {
+                                                        // Update timeOfLastDose for backward compatibility
+                                                        achievement.setTimeOfLastDose(System.currentTimeMillis());
+                                                        
+                                                        // Update streak (async - based on ControllerSchedule dates)
+                                                        // calculateStreakFromSchedule will update currentStreak in its callback
+                                                        achievement.calculateStreakFromSchedule(new ResultCallBack<Integer>() {
                                                             @Override
-                                                            public void onComplete() {
-                                                                startActivity(new Intent(ChildInhalerUseAfter.this, ChildInhalerMenu.class));
+                                                            public void onComplete(Integer streak) {
+                                                                // Streak has been updated, now check for badge and save
+                                                                if (!achievement.badges.get(0)) {
+                                                                    if (achievement.checkBadge1()) {
+                                                                        Toast.makeText(ChildInhalerUseAfter.this, "You've earned a new badge!", Toast.LENGTH_SHORT).show();
+                                                                        achievement.badges.set(0, true);
+                                                                    }
+                                                                }
+                                                                
+                                                                AchievementsModel.writeIntoDB(achievement, new CallBack() {
+                                                                    @Override
+                                                                    public void onComplete() {
+                                                                        startActivity(new Intent(ChildInhalerUseAfter.this, ChildInhalerMenu.class));
+                                                                    }
+                                                                });
                                                             }
                                                         });
                                                     }
